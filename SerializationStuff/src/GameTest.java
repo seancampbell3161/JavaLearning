02f16fa6ad5bcc2ab5061d4c012e97297538e780
Sean Campbell <sean.campbell3161@gmail.com>
2021-06-22 15:10:50 -0500

    add serialization test

---
+++ SerializationStuff/src/GameTest.java
@@ -0,0 +1,46 @@
+import java.io.*;
+
+public class GameTest {
+
+    public static void main(String[] args) {
+
+
+        GameCharacter one = new GameCharacter(50, "Elf", new String[] {"bow", "sword", "dust"});
+        GameCharacter two = new GameCharacter(200, "Troll", new String[] {"bare hands", "axe"});
+        GameCharacter three = new GameCharacter(120, "Wizard", new String[] {"spells", "invsibility"});
+
+        try {
+            ObjectOutputStream os = new ObjectOutputStream(new FileOutputStream("Game.ser"));
+            os.writeObject(one);
+            os.writeObject(two);
+            os.writeObject(three);
+        } catch (FileNotFoundException e) {
+            e.printStackTrace();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+        one = null;
+        two = null;
+        three = null;
+
+        try {
+            ObjectInputStream is = new ObjectInputStream(new FileInputStream("Game.ser"));
+            GameCharacter oneRestore = (GameCharacter) is.readObject();
+            GameCharacter twoRestore = (GameCharacter) is.readObject();
+            GameCharacter threeRestore = (GameCharacter) is.readObject();
+
+            // check to see if it worked
+            System.out.println("One's type: " + oneRestore.getType());
+            System.out.println("Two's type: " + twoRestore.getType());
+            System.out.println("Three's type: " + threeRestore.getType());
+
+        } catch (FileNotFoundException e) {
+            e.printStackTrace();
+        } catch (IOException e) {
+            e.printStackTrace();
+        } catch (ClassNotFoundException e) {
+            e.printStackTrace();
+        }
+    }
+
+}
