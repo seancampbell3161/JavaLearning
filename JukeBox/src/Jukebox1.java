e07594885bb45c1df3012c32d745770fbdec8fd2
Sean Campbell <sean.campbell3161@gmail.com>
2021-06-24 12:22:36 -0500

    create class that will read from a file and parse the song name only

---
+++ JukeBox/src/Jukebox1.java
@@ -0,0 +1,37 @@
+import java.io.*;
+import java.util.ArrayList;
+
+public class Jukebox1 {
+
+    public ArrayList<String> songList = new ArrayList<>();
+
+    public static void main(String[] args) {
+        Jukebox1 j1 = new Jukebox1();
+        j1.go();
+    }
+
+    public void go() {
+        getSongs();
+        System.out.println(songList);
+    }
+
+    public void getSongs() {
+
+        try {
+            File file = new File("songs.txt");
+            BufferedReader reader = new BufferedReader(new FileReader(file));
+            String line = null;
+            while((line = reader.readLine()) != null) {
+                addSong(line);
+            }
+        } catch(Exception e) {
+            e.printStackTrace();
+        }
+        
+    }
+
+    public void addSong(String s) {
+        String[] songAndArtist = s.split("/");
+        songList.add(songAndArtist[0]);
+    }
+}
